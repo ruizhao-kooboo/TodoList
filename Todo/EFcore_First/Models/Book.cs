ec97851a03e283af8a2b86c3e028bba6cbbd94a6
ruizhao-kooboo <ruizhao@kooboo.cn>
2022-06-17 15:38:23 +0800

    创建图书类

---
+++ Todo/EFcore_First/Models/Book.cs
@@ -0,0 +1,14 @@
+﻿namespace EFcore_First.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime PublieshedOn { get; set; }
+
+        public string Publisher { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
