e2bd050329c2c53be4713b418e517aaadcbc6784
ruizhao-kooboo <ruizhao@kooboo.cn>
2022-06-17 15:39:47 +0800

    创建一个EFcore first test

---
+++ Todo/EFcore_First/Context.cs
@@ -0,0 +1,30 @@
+﻿using EFcore_First.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFcore_First
+{
+    //可以把上下文类看成一个数据库
+    public class Context:DbContext
+    {
+        //主要是框架再用，有时用户也可能用到
+        public Context()
+        {
+
+        }
+
+        public Context(DbContextOptions<Context> options)
+        {
+
+        }
+
+        public DbSet<Book> Book { get; set; }
+
+        //配置上下文类
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connString = @"server=PC - 20220531UTCR;database=efcoreDb;MultipleActvieResultSets=true";
+            optionsBuilder.UseSqlServer(connString);
+        }
+
+    }
+}
---
+++ Todo/EFcore_First/Migrations/20220617073128_init-Mig.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFcore_First.Migrations
+{
+    public partial class initMig : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Book",
+                columns: table => new
+                {
+                    BookId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PublieshedOn = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Publisher = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Book", x => x.BookId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Book");
+        }
+    }
+}
