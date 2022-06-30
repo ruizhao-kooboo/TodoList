1db26336abe0a62b2a7d8cbd6af319f2705902c3
ruizhao-kooboo <ruizhao@kooboo.cn>
2022-06-30 15:56:58 +0800

    B站IOC: https://www.bilibili.com/video/BV16B4y1c7KX?p=2&spm_id_from=pageDriver&vd_source=b5d81e547f7e01a8d43326da2b0f3406

---
+++ Todo/AspNetCoreBasic_1/IocShow.cs
@@ -0,0 +1,72 @@
+﻿/// 本节：DI（Dependency Injecation） -IOC（Inversion of Control）--依赖倒置原则（效果）
+/// 依赖倒置原则：
+/// 1、上层模块不应该依赖于底层模块，它们都应该依赖于抽象。
+/// 2、抽象不应该依赖于细节，细节应该依赖于抽象
+
+namespace AspNetCoreBasic_1
+{
+    public class IocShow
+    {
+        public void Show()
+        {
+            //Student student = new Student();
+            IPerson person = new Student();
+            person.PlayGames();
+        }
+    }
+
+    /// <summary>
+    /// 工厂
+    /// </summary>
+    class Factory
+    {
+        public static IPerson CreateInstance()
+            { return new Teacher(CreateMessage()); }
+
+        public static IMessage CreateMessage()
+        { return new Message(); }
+    }
+
+    interface IPerson 
+    {
+        void PlayGames();
+    }
+
+    class Student : IPerson
+    {
+        public void PlayGames()
+        {
+            Console.WriteLine("hello");
+        }
+    }
+
+    class Teacher : IPerson
+    {
+        private IMessage _message;
+        public Teacher(IMessage message)
+        {
+            _message = message;
+        }
+
+        public void PlayGames()
+        {
+            //Message message = new Message();
+            //IMessage message = Factory.CreateMessage();
+            _message.SendMessage("teacher","morning");
+            Console.WriteLine("i am a teacher");
+        }
+    }
+
+    public interface IMessage
+    {
+        void SendMessage(string person, string message);
+    }
+
+    public class Message : IMessage
+    {
+        public void SendMessage(string person, string message)
+        {
+            Console.WriteLine($"{person}{message}");
+        }
+    }
+}
---
+++ Todo/AspNetCoreBasic_1/Program.cs
@@ -0,0 +1,11 @@
+﻿using AspNetCoreBasic_1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        IocShow iocShow = new IocShow();
+        iocShow.Show();
+        Console.WriteLine("World");
+    }
+}
